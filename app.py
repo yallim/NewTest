536f0d3eb249b8339a551205fd748a783ac30510
cassie <cassie@clover.games>
2024-12-16 11:17:56 +0900

    com

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/<string_value>')
 def show_string(string_value):
     app.logger.info(f"Received string_value: {string_value}")
+    print("DEV$")
     return show_strings(string_value)
 
 if __name__ == '__main__':
