4214719f8f830b65762c213bfe71a6a9d1106ce3
cassie <cassie@clover.games>
2024-12-27 18:40:06 +0900

    add

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/<string_value>')
 def show_string(string_value):
     app.logger.info(f"Received string_value: {string_value}")
-    print("DEV4!")
+    print("새로추가할거~를 지움")
+    print("new")
     return show_strings(string_value)
 
 if __name__ == '__main__':
