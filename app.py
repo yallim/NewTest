e500a1a1d0f0941d2ba2c33c9042d2c53110aa95
cassie <cassie@clover.games>
2025-04-24 14:03:18 +0900

    com

--- app.py
+++ app.py
@@ -10,12 +10,7 @@ def show_string(string_value):
     app.logger.info(f"Received string_value: {string_value}")
     print("release만의작업")
     print("열작업중")
-    print("열작업중2")
-    print("열작업중3")
-    print("열작업중4")
-    print("열작업중5")
-    print("열작업중6")
-    print("열작업중7")
+    
     return show_strings(string_value)
 
 
