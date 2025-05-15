83253df87fd0042ba1f8f576b576fa5fecebe38b
cassie <cassie@clover.games>
2025-05-15 16:12:25 +0900

    com222

---
+++ tapp.py
@@ -0,0 +1,14 @@
+import json
+import html
+
+with open('game-avalon-world.bytes', 'rb') as f:
+    byte_data = f.read()
+
+# cp949로 디코딩딩
+decoded_str = html.unescape(byte_data.decode('cp949'))  # 또는 'euc-kr'
+
+# JSON 파싱
+json_data = json.loads(decoded_str)
+
+print(json_data)
+
